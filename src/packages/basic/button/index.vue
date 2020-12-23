e53bcac60fa7a1cc83e11f57f5750f899540df8f
nosillytrying_tang <nosillytrying_tang@163.com>
2020-12-23 16:22:03 +0800

    修改属性

--- src/packages/basic/button/index.vue
+++ src/packages/basic/button/index.vue
@@ -22,29 +22,35 @@ export default defineComponent({
     position: {
       type: String as PropType<IButtonPosition>,
       default: 'left',
-      validator (position: IButtonPosition): boolean {
-        if (!positionList.includes(position)) {
-          throw new Error('button\'s position is only:' + sizeList.join('、'))
+      ...{
+        validator (position: IButtonPosition): boolean {
+          if (!positionList.includes(position)) {
+            throw new Error('button\'s position is only:' + sizeList.join('、'))
+          }
+          return true
         }
-        return true
       }
     },
     size: {
       type: String as PropType<IButtonSize>,
-      validator (size: IButtonSize): boolean {
-        if (!sizeList.includes(size)) {
-          throw new Error('button\'s size is only:' + sizeList.join('、'))
+      ...{
+        validator (size: IButtonSize): boolean {
+          if (!sizeList.includes(size)) {
+            throw new Error('button\'s size is only:' + sizeList.join('、'))
+          }
+          return true
         }
-        return true
       }
     },
     type: {
       type: String as PropType<IButtonType>,
-      validator (type: IButtonType): boolean {
-        if (!typeList.includes(type)) {
-          throw new Error('button\'s type is only:' + typeList.join('、'))
+      ...{
+        validator (type: IButtonType): boolean {
+          if (!typeList.includes(type)) {
+            throw new Error('button\'s type is only:' + typeList.join('、'))
+          }
+          return true
         }
-        return true
       }
     },
     plain: {
@@ -79,28 +85,27 @@ export default defineComponent({
   },
   setup (props) {
     const computedClass = computed(() => {
-      const propsType = (props as ButtonProps)
       const ret = ['zebra-button']
-      if (propsType.type) {
-        ret.push(`zebra-button--${propsType.type}`)
+      if (props.type) {
+        ret.push(`zebra-button--${props.type}`)
       }
       // if (props.position) {
       //   ret.push(`zebra-button--${props.position}`);
       // }
-      if (propsType.round) {
+      if (props.round) {
         ret.push('is-round')
       }
-      if (propsType.plain) {
+      if (props.plain) {
         ret.push('is-plain')
       }
-      if (propsType.disabled || propsType.loading) {
+      if (props.disabled || props.loading) {
         ret.push('is-disabled')
       }
-      if (propsType.circle) {
+      if (props.circle) {
         ret.push('is-circle')
       }
-      if (propsType.size) {
-        ret.push(`zebra-button--${propsType.size}`)
+      if (props.size) {
+        ret.push(`zebra-button--${props.size}`)
       }
       return ret
     })
